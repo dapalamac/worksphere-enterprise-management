f4a2d863ec4c3ead434fdb37a8d99c7ae6f4195b
Janus <David Palacio>
2026-07-13 11:05:28 -0500

    Agregar archivos de proyecto.

---
+++ WorkSphere.Domain/Entities/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkSphere.Domain.Common;
+
+namespace WorkSphere.Domain.Entities;
+public class Employee : BaseEntity
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Phone { get; set; } = string.Empty;
+
+    public DateOnly BirthDate { get; set; }
+
+    public DateOnly HireDate { get; set; }
+
+    public decimal Salary { get; set; }
+
+    public Guid DepartmentId { get; set; }
+
+    public Guid PositionId { get; set; }
+}
---
+++ WorkSphere.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkSphere.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
